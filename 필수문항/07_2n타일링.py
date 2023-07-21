7513acb9976847f6e539cd40d187b517cdd99f2a
hyunsik <hyunsig96@gmail.com>
2023-07-21 10:25:18 +0900

    fix: 07

--- 필수문항/07_2n타일링.py
+++ 필수문항/07_2n타일링.py
@@ -3,13 +3,15 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 
-dp = [0] * (n + 1)
+if n < 3:
+    print(n)
+else:
+    dp = [0] * (n + 1)
 
-dp[1] = 1
-dp[2] = 2
+    dp[1] = 1
+    dp[2] = 2
 
-if n > 2:
     for i in range(3, n + 1):
         dp[i] = dp[i - 2] + dp[i - 1]
 
-print(dp[n] % 10007)
+    print(dp[n] % 10007)
